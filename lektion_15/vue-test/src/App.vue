65a307248a022bf196da8ede799a83b230f1e83a
Hampus Ram <hampus.ram@educ.goteborg.se>
2025-04-29 13:12:08 +0200

    lektion 15

---
+++ lektion_15/vue-test/src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import MyComponent from './components/MyComponent.vue'
+</script>
+
+<template>
+  <h1>Titel</h1>
+  <MyComponent />
+  <button>gör inget alls!</button>
+</template>
+
+<style scoped>
+h1 {
+  color: blue;
+}
+</style>
---
+++ lektion_15/vue-test/src/components/MyComponent.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <h2>Testing testing</h2>
+
+        <p>{{ message }}</p>
+
+        <button @click="changeMessage">click me</button>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            message: "Hello, World!"
+        }
+    },
+    methods: {
+        changeMessage() {
+            this.message = "Goodbye!"
+        }
+    }
+}
+</script>
+
+<style scoped>
+button {
+    font-family: 'Comic Sans MS';
+    font-weight: 700;
+}
+</style>
